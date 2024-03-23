5856180c7b7db9ed0635b048af899b0ff758cd8d
egosy <65727224+egosy@users.noreply.github.com>
2024-03-24 00:35:30 +0300

    Update db.go

--- TestTask/main/db.go
+++ TestTask/main/db.go
@@ -22,7 +22,7 @@ func ConnectDB() (*mongo.Client, context.CancelFunc) {
 
 // make async
 func UpdateUserRFTokenHash(id string, newHash []byte) {
-	coll := client.Database("mydb").Collection("users")
+	coll := client.Database("dbUser").Collection("users")
 
 	res := base64.StdEncoding.EncodeToString(newHash)
 	update := bson.D{{Key: "$set", Value: bson.D{{Key: "rf_token_hash", Value: res}}}}
@@ -35,7 +35,7 @@ func UpdateUserRFTokenHash(id string, newHash []byte) {
 }
 
 func AddUser(id string, newHash []byte) {
-	coll := client.Database("mydb").Collection("users")
+	coll := client.Database("dbUser").Collection("users")
 
 	res := base64.StdEncoding.EncodeToString(newHash)
 	_, err := coll.InsertOne(context.TODO(), User{ID: id, RFTokenHash: res})
@@ -45,7 +45,7 @@ func AddUser(id string, newHash []byte) {
 }
 
 func IsUserExist(id string) bool {
-	coll := client.Database("mydb").Collection("users")
+	coll := client.Database("dbUser").Collection("users")
 	err := coll.FindOne(context.TODO(), bson.D{{Key: "id", Value: id}}).Err()
 
 	if err == mongo.ErrNoDocuments {
@@ -58,7 +58,7 @@ func IsUserExist(id string) bool {
 }
 
 func IsHashExistsInDB(hash []byte) bool {
-	coll := client.Database("mydb").Collection("users")
+	coll := client.Database("dbUser").Collection("users")
 
 	res := base64.StdEncoding.EncodeToString(hash)
 	err := coll.FindOne(context.TODO(), bson.D{{Key: "rf_token_hash", Value: res}}).Err()
